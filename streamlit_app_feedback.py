61f021808c4e3881d64f210c86c14784a04cc8b0
Malo-Aymes <120594447+Malo-Aymes@users.noreply.github.com>
2023-04-05 13:33:43 +0200

    Update streamlit_app_feedback.py

--- streamlit_app_feedback.py
+++ streamlit_app_feedback.py
@@ -209,9 +209,8 @@ if is_pressed["pressed"]and user_input:
     ##  
     
         df_test = pd.read_csv("https://raw.githubusercontent.com/Malo-Aymes/Streamlit/main/BdD1.csv",sep=";",encoding= 'unicode_escape',error_bad_lines=False)
-      
-       # st.write(df_test.columns[:12].tolist())
     ##
+        st.write(df_test.shape)
 
         df_test["labels"] = df_test[labels].values.tolist()
 
